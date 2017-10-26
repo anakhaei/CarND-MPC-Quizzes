ea11c26818f860da0f84ddbacaedd8c63b89a68b
Alireza <a.nakhaei@gmail.com>
2017-10-26 09:55:43 -0700

    added constraints to MPC

--- mpc_to_line/src/MPC.cpp
+++ mpc_to_line/src/MPC.cpp
@@ -11,8 +11,8 @@ namespace plt = matplotlibcpp;
 using CppAD::AD;
 
 // TODO: Set N and dt
-size_t N = ? ;
-double dt = ? ;
+size_t N = 20 ;
+double dt = 0.2 ;
 
 // This value assumes the model presented in the classroom is used.
 //
@@ -80,10 +80,19 @@ class FG_eval {
     // The rest of the constraints
     for (int t = 1; t < N; t++) {
       AD<double> x1 = vars[x_start + t];
+      AD<double> y1 = vars[y_start + t];
+      AD<double> psi1 = vars[psi_start + t];
+      AD<double> v1 = vars[vstart + t];
+ 
 
       AD<double> x0 = vars[x_start + t - 1];
       AD<double> psi0 = vars[psi_start + t - 1];
       AD<double> v0 = vars[v_start + t - 1];
+      AD<double> y0 = vars[y_start + t - 1];
+      //AD<double> cte0= vars[cte_start + t - 1];
+      //AD<double> epsi0 = vars[epsi_start + t - 1];
+      AD<double> a0 = vars[a_start + t];
+      AD<double> delta0 = vars [delta_start + 1]; 
 
       // Here's `x` to get you started.
       // The idea here is to constraint this value to be 0.
@@ -94,6 +103,9 @@ class FG_eval {
 
       // TODO: Setup the rest of the model constraints
       fg[1 + x_start + t] = x1 - (x0 + v0 * CppAD::cos(psi0) * dt);
+      fg[1 + y_start + t] = y1 - (y0 + v0 * CppAD::sin(psi0) * dt);
+      fg[1 + phi_start + t] = phi1 - (phi0 + v0 / Lf * delta0 * dt);
+      fg[1 + v_start + t] = v1 - a0 * dt;
     }
   }
 };
